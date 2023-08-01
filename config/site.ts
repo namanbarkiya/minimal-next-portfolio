c7a69420fde1b86c451915ca2cd0d6a60ca5b367
Naman <naman.barkiya02@gmail.com>
2023-08-01 21:08:26 +0530

    Initial setup with Next.js and shadcn/ui

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig: any = {
+    name: "Naman Barkiya",
+    description:
+        "An open source application built using the new router, server components and everything new in Next.js 13.",
+    url: "https://namanbarkiya.com",
+    ogImage: "https://namanbarkiya.com/og.jpg",
+    links: {
+        twitter: "https://twitter.com/naman",
+        github: "https://github.com/naman-barkiya",
+    },
+};
