a3933373b0581371d717bc2f2f118b4d28750f0e
Naman <naman.barkiya02@gmail.com>
2023-08-21 05:05:08 +0530

    env checking

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Metadata } from "next";
 
 import PageHeader from "@/components/page-header";
 import { ContactForm } from "@/components/forms/contact-form";
 import { pagesConfig } from "@/config/pages";
 
+export const metadata: Metadata = {
+    title: "Contact",
+    description: "Examples of cards built using the components.",
+};
+
 export default function ContactPage() {
+    console.log(process.env.NODEMAILER_EMAIL);
+    console.log(process.env.NODEMAILER_PWD);
     return (
         <>
             <PageHeader
