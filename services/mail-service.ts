01aa7abe5437eac0639d27f2a642b2d9e0d193fe
Naman <naman.barkiya02@gmail.com>
2023-08-21 06:28:54 +0530

    sendgrid integration

--- services/mail-service.ts
+++ services/mail-service.ts
@@ -1,4 +1,6 @@
-let nodemailer = require("nodemailer");
+// let nodemailer = require("nodemailer");
+
+const sgMail = require("@sendgrid/mail");
 
 export async function sendMail(
     name: string,
@@ -6,29 +8,43 @@ export async function sendMail(
     socialLink: string,
     email: string
 ) {
-    var transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.NODEMAILER_EMAIL,
-            pass: process.env.NODEMAILER_PWD,
-        },
-    });
-
-    var mailOptions = {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
         from: process.env.NODEMAILER_EMAIL,
-        to: "naman.barkiya02@gmail.com", // my email to recieve message
+        to: "naman.barkiya02@gmail.com",
         subject: `Portfolio: ${name} sent you a message`,
         text: `message from ${name}: ${message}. Email: ${email}, Social Link: ${socialLink}`,
     };
-
-    await new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, function (err: any, info: any) {
-            if (err) {
-                console.error(err);
-                reject(err);
-            } else {
-                resolve(info);
-            }
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log("Email sent !!");
+        })
+        .catch((error: any) => {
+            console.error(error);
         });
-    });
+
+    // var transporter = nodemailer.createTransport({
+    //     service: "gmail",
+    //     auth: {
+    //         user: process.env.NODEMAILER_EMAIL,
+    //         pass: process.env.NODEMAILER_PWD,
+    //     },
+    // });
+
+    // var mailOptions = {
+    //     from: process.env.NODEMAILER_EMAIL,
+    //     to: "naman.barkiya02@gmail.com", // my email to recieve message
+    //     subject: `Portfolio: ${name} sent you a message`,
+    //     text: `message from ${name}: ${message}. Email: ${email}, Social Link: ${socialLink}`,
+    // };
+
+    // transporter.sendMail(mailOptions, function (error: any, info: any) {
+    //     if (error) {
+    //         throw new Error(error);
+    //     } else {
+    //         console.log("Email Sent");
+    //         return true;
+    //     }
+    // });
 }
