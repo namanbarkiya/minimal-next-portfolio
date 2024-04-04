b11b141591b8334e40790df19464203259035ffc
Naman <naman.barkiya02@gmail.com>
2024-04-04 22:54:09 +0530

    Navbar positioning

--- config/routes.ts
+++ config/routes.ts
@@ -1,20 +1,20 @@
 export const routesConfig: any = {
-  mainNav: [
-    {
-      title: "Skills",
-      href: "/skills",
-    },
-    {
-      title: "Experience",
-      href: "/experience",
-    },
-    {
-      title: "Contact",
-      href: "/contact",
-    },
-    {
-      title: "Contributions",
-      href: "/contributions",
-    },
-  ],
+    mainNav: [
+        {
+            title: "Skills",
+            href: "/skills",
+        },
+        {
+            title: "Experience",
+            href: "/experience",
+        },
+        {
+            title: "Contributions",
+            href: "/contributions",
+        },
+        {
+            title: "Contact",
+            href: "/contact",
+        },
+    ],
 };
