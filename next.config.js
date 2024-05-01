a8f3f1363894b8431e868b39aba664000e00c811
Naman <naman.barkiya02@gmail.com>
2024-05-01 20:32:58 +0530

    singlebit contact route added (personal)

--- next.config.js
+++ next.config.js
@@ -8,6 +8,28 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: "/api/sb-contact",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "https://singlebit.xyz",
+                    }, // for singlebit website contact form
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,DELETE,PATCH,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
