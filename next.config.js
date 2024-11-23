37eb1408f5039bd8723174a3b16e63435378aff9
Naman <naman.barkiya02@gmail.com>
2024-11-23 23:55:42 +0530

    config cleanup

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.unsplash.com",
-            },
-        ],
-    },
     async headers() {
         return [
             {
                 source: "/api/sb-contact",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    {
-                        key: "Access-Control-Allow-Origin",
-                        value: "https://singlebit.xyz",
-                    }, // for singlebit website contact form
                     {
                         key: "Access-Control-Allow-Methods",
                         value: "GET,DELETE,PATCH,POST,PUT",
