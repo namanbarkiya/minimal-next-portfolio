9e826ea52dd55453527dc16cf159503d9ec608b4
Naman <naman.barkiya02@gmail.com>
2026-03-17 20:44:19 +0530

    Convot added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Inter as FontSans } from "next/font/google";
 import localFont from "next/font/local";
+import Script from "next/script";
 
 import { Analytics } from "@/components/common/analytics";
 import { ThemeProvider } from "@/components/common/theme-provider";
@@ -130,6 +131,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <Toaster />
           <ModalProvider />
         </ThemeProvider>
+        <Script
+          src="https://convot.xyz/widget.js"
+          data-token="3vpr28Va7E8luRq8DMOStAr9tefOCVqifQ28fpp6grrKS4zflNRZQjQpmeu4os_2nuLmmh1DOshndiN5O1vvGg"
+          data-api-url="https://api.convot.xyz"
+          strategy="afterInteractive"
+        />
       </body>
       <GoogleAnalytics gaId={GA_ID} />
     </html>
